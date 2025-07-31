3624b0085b6c9a3efa337ea990c914d482d0e2c9
garzzaro <barbaragarzaro06@gmail.com>
2025-07-31 10:02:36 -0600

    Update EdificioControler.java

--- EdificioControler.java
+++ EdificioControler.java
@@ -1,34 +1,74 @@
 public class EdificioControler {
-    //Atributos
-    private  Inventario Inventario;
-    private Habitaciones Habitaciones;
-    private Edificio Edificio;
-    private EdificioVista vista; 
+    // Atributos
+    private Inventario inventario;
+    private Habitaciones habitaciones;
+    private Edificio edificio;
+    private EdificioVista vista;
 
-    public EdificioControler(EdificioVista vista){
+    // Constructor
+    public EdificioControler(EdificioVista vista) {
         this.vista = vista;
         inicializarEdificio();
-
-    }
-    private void inicializarEdificio(){
-        Inventario
-        Habitaciones
-        Edificio
     }
 
-    //Constructor
-    public Edificio(String nombreOrfanato, String ubicacionOrfanato, int numeroNinos) {
-        this.nombreOrfanato = nombreOrfanato;
-        this.ubicacionOrfanato = ubicacionOrfanato;
-        this.numeroNinos = numeroNinos;
-    }
-    //Metodos
-    public void verDatos() {
-        System.out.println("Nombre del orfanato: " + nombreOrfanato);
-        System.out.println("Ubicación: " + ubicacionOrfanato);
+    private void inicializarEdificio() {
+      
+        String nombreOrfanato = vista.pedirTexto("Ingresar el nombre del orfanato: ");
+        String ubicacionOrfanato = vista.pedirTexto("Ingresar la ubicación del orfanato: ");
+        int numeroNinos = vista.pedirEntero("Ingresar el número de niños en el orfanato: ");
+        int pisos = vista.pedirEntero("Ingresar el número de pisos del orfanato: ");
+        edificio = new Edificio(nombreOrfanato, ubicacionOrfanato, numeroNinos, pisos);
+    
+        String nombre = vista.pedirTexto("Ingresar alimento o artículo: ");
+        int cantidadInventario = vista.pedirEntero("Ingresar la cantidad: ");
+        inventario = new Inventario(nombre, cantidadInventario);
+
+        int numCuarto = vista.pedirEntero("Ingrese el número de cuarto: ");
+        int personasHabitacion = vista.pedirEntero("Ingrese número de personas en la habitación: ");
+        int camas = vista.pedirEntero("Ingrese número de camas: ");
+        habitaciones = new Habitaciones(numCuarto, personasHabitacion, camas);
     }
 
-    public void getNumeroNinos() {
-        System.out.println("Número de niños en el orfanato: " + numeroNinos);
+    public void ejecutarMenu() {
+        int opcion;
+        do {
+            vista.mostrarMenu();
+            opcion = vista.pedirOpcion();
+
+            switch (opcion) {
+                case 1:
+                    inventario.existencia();
+                    break;
+                case 2:
+                    inventario.donaciones();
+                    break;
+                case 3:
+                    habitaciones.getCamas();
+                    break;
+                case 4:
+                    habitaciones.getPersonasHabitacion();
+                    break;
+                case 5:
+                    edificio.verDatos();
+                    break;
+                case 6:
+                    edificio.getNumeroNinos();
+                    break;
+                case 7:
+                    edificio.getPisos();
+                    break;
+                case 8:
+                    habitaciones.getNumCuarto();
+                    break;
+                case 9:
+                    inventario.getRopa();
+                    break;
+                case 10:
+                    System.out.println("Saliendo del menú...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente nuevamente.");
+            }
+        } while (opcion != 10);
     }
 }
